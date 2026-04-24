f0179812d261a88bda8819807542a474fc2c1f16
Rodrigo Francisco Pettinati Mikaro <rodrigomikaro@outlook.com.br>
2026-04-24 10:13:11 -0300

    feat: second traffic light implemented

--- sketch.ino
+++ sketch.ino
@@ -4,11 +4,18 @@
 #define led_yellow_1 3 
 #define led_red_1 4
 
+#define led_green_2 5
+#define led_yellow_2 6
+#define led_red_2 7
+
 
 void setup() {
   pinMode(led_green_1, OUTPUT);
   pinMode(led_yellow_1, OUTPUT);
   pinMode(led_red_1, OUTPUT);
+  pinMode(led_green_2, OUTPUT);
+  pinMode(led_yellow_2, OUTPUT);
+  pinMode(led_red_2, OUTPUT);
 }
 
 
@@ -16,20 +23,32 @@ void loop() {
   digitalWrite(led_green_1, 1);
   digitalWrite(led_yellow_1, 0);
   digitalWrite(led_red_1, 0);
+  digitalWrite(led_green_2, 0);
+  digitalWrite(led_yellow_2, 0);
+  digitalWrite(led_red_2, 1);
   delay(5000);
 
   digitalWrite(led_green_1, 0);
   digitalWrite(led_yellow_1, 1);
   digitalWrite(led_red_1, 0);
+  digitalWrite(led_green_2, 0);
+  digitalWrite(led_yellow_2, 0);
+  digitalWrite(led_red_2, 1);  
   delay(1000);
 
   digitalWrite(led_green_1, 0);
   digitalWrite(led_yellow_1, 0);
   digitalWrite(led_red_1, 1);
+  digitalWrite(led_green_2, 1);
+  digitalWrite(led_yellow_2, 0);
+  digitalWrite(led_red_2, 0);  
   delay(5000);
 
   digitalWrite(led_green_1, 0);
   digitalWrite(led_yellow_1, 0);
   digitalWrite(led_red_1, 1);
+  digitalWrite(led_green_2, 0);
+  digitalWrite(led_yellow_2, 1);
+  digitalWrite(led_red_2, 0);  
   delay(1000);
 }
